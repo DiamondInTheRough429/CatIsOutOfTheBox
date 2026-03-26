afe1fcf3e6120349932910b07fb3193dafd15568
Diamond <diamondintherough42903@gmail.com>
2026-03-26 18:47:13 -0400

    finish cosmetic, started totorial labels T1 ready t2 and 3 have labels but no text

---
+++ Assets/UniversalAssets.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name UniversalAssets
+
+const  WASDMOVEPATH:String = "uid://dhky3awtgdxc7"
+const ARROWMOVEPATH:String = "uid://cpbgpqooj78t0"
+const DPADMOVEPATH:String = "uid://bvptp318lyiqp"
+const JOYSTICKMOVEPATH:String = "uid://wwe0cxk0vwua"
--- Menus/Settings/CosmeticButton.gd
+++ Menus/Settings/CosmeticButton.gd
@@ -2,9 +2,32 @@ extends Button
 class_name CosmeticButton
 
 @export var CosmeticOfButton:SettingsHandler.Cosmetics
-
-@export var CosmeticToIcon:Dictionary[SettingsHandler.Cosmetics, Texture2D]
+@export var LockedIcon:Texture2D
+@export var CurrentLabel:Label
+@export var CosmeticToIcon:Dictionary[SettingsHandler.Cosmetics, Texture2D] ={
+	SettingsHandler.Cosmetics.DEFAULT_COSMETIC : null,
+	SettingsHandler.Cosmetics.GOJO_COSMETIC : null,
+	SettingsHandler.Cosmetics.JELLIE_COSMETIC : null,
+	SettingsHandler.Cosmetics.SNAKE_COSMETIC : null,
+	SettingsHandler.Cosmetics.ROBOTCAT_COSMETIC : null,
+	SettingsHandler.Cosmetics.RAT_COSMETIC : null,
+	SettingsHandler.Cosmetics.CATCAT_COSMETIC : null,
+	SettingsHandler.Cosmetics.DEVART_COSMETIC : null 
+}
 
 func _ready() -> void:
-	icon = CosmeticToIcon.get(CosmeticOfButton)
-	text = SettingsHandler.Cosmetics.keys().get(CosmeticOfButton)
+	UpdateLook()
+	pressed.connect(Choose)
+
+func UpdateLook() -> void:
+	if SaveHandler.GetCosmeticUnlocked(CosmeticOfButton):
+		icon = CosmeticToIcon.get(CosmeticOfButton)
+		text = SettingsHandler.Cosmetics.keys().get(CosmeticOfButton)
+	else:
+		icon = LockedIcon
+		text = "NOT_UNLOCKED_COSMETIC"
+		disabled = true
+	CurrentLabel.visible = true if SettingsHandler.CurrentCosmetic == CosmeticOfButton else false
+
+func Choose() -> void:
+	SettingsHandler.CurrentCosmetic = CosmeticOfButton
