f9eac4c70d2b12e739bde09c7a8aef7eb419d2f3
Diamond <diamondintherough42903@gmail.com>
2026-03-28 14:41:47 +0000

    Main Menu Stuff and levels wierd

--- Menus/MainManu/MainScreenBackground.gd
+++ Menus/MainManu/MainScreenBackground.gd
@@ -4,16 +4,16 @@ extends TileMapLayer
 @export var RandomTimer:Timer
 
 func _ready() -> void:
-	RandomTimer.start(randi_range(1, 15))
+	RandomTimer.start(randf_range(.1, 2))
 	RandomTimer.timeout.connect(AnimateFakePlayer)
 
 func AnimateFakePlayer() -> void:
 	FakePlayer.position = Vector2(-16, randi_range(1, 20)*16)
-	for i in 320:
+	for i in 22:
 		var NewPosition = FakePlayer.position
 		NewPosition.x += 32
 		FakePlayer.play("MoveRight")
 		var MoveTween:Tween = create_tween()
 		MoveTween.tween_property(FakePlayer, "position", NewPosition, .1)
 		await MoveTween.finished
-	RandomTimer.start(randi_range(1, 15))
+	RandomTimer.start(randf_range(.1, 2))
